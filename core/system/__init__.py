7b049f5ce2d02700e511becd0621a5d813b0e75d
MOKSHA2025 <mokshaarchitectofficial@gmail.com>
2026-08-31 09:16:27 +0530

    sync: MKD development update 2026-08-31 09:16:27

---
+++ core/system/__init__.py
@@ -0,0 +1,13 @@
+from .system import (
+    MKD_NAME,
+    MKD_VERSION,
+    MKD_STATUS,
+    get_system_info,
+)
+
+__all__ = [
+    "MKD_NAME",
+    "MKD_VERSION",
+    "MKD_STATUS",
+    "get_system_info",
+]
---
+++ core/system/system.py
@@ -0,0 +1,16 @@
+"""
+MKD System Core
+MOKSHA KERNEL DESKTOP EXPERIMENT
+"""
+
+MKD_NAME = "MOKSHA KERNEL DESKTOP EXPERIMENT"
+MKD_VERSION = "0.1.0"
+MKD_STATUS = "FOUNDATION"
+
+
+def get_system_info():
+    return {
+        "name": MKD_NAME,
+        "version": MKD_VERSION,
+        "status": MKD_STATUS,
+    }
